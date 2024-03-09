c5857dbbe4786865a1cff120dad0211bc168fb4a
josssss07 <joshuadesai03@gmail.com>
2024-03-09 10:18:51 +0530

    hehe

--- prac_4/lib/bmi.dart
+++ prac_4/lib/bmi.dart
@@ -1,18 +1,12 @@
-import 'dart:js';
+
 
 import 'package:flutter/material.dart';
 
 class BMIPage extends StatelessWidget {
   const BMIPage({super.key});
 
-  void showbMI (double bmi){
-    showDialog(context: context, builder: (context){
-      AlertDialog(
-        actions: [],
-      )
-    })
 
-  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
